46d3606752ad019f3a3fb557d0b49414c3654197
Aman Nindra <amannindra@gmail.com>
2024-03-23 12:26:45 -0700

    not done

--- Drumbeat.js
+++ Drumbeat.js
@@ -1,4 +1,38 @@
+var soundBoom = document.getElementById("boom");
+var soundClap = document.getElementById("clap");
+var soundhiHat = document.getElementById("hihat");
+var soundKick = document.getElementById("kick");
+var soundHat = document.getElementById("hat");
+var soundRide = document.getElementById("ride");
+var soundSnare = document.getElementById("snare");
+var soundTink = document.getElementById("tink");
+var soundTom = document.getElementById("tom");
+
+
 function playboom() {
-    var sound = document.getElementById("boom");
-    sound.play();
+  soundBoom.play();
   }
+function playclap(){
+  soundClap.play();
+}
+function playhihat(){
+  soundhiHat.play();
+}
+function playkick(){
+  soundKick.play();
+}
+function playhat(){
+  soundHat.play();
+}
+function playride(){
+  soundRide.play();
+}
+function playsnare(){
+  soundSnare.play();
+}
+function playtink(){
+  soundTink.play();
+}
+function playtom(){
+  soundTom.play();
+}
